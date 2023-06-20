9b276c7ee2142620a025fc44d2bbfcf819439788
Moixie <royelpos@gmail.com>
2023-06-20 22:22:03 +0800

    Announcement Page

--- resources/js/Pages/Announcement/Index.vue
+++ resources/js/Pages/Announcement/Index.vue
@@ -56,137 +56,26 @@ defineProps({
             <div class=" max-w-7xl px-6 lg:px-8">
                   <!-- max-auto removed here -->
               <div class=" max-w-1xl lg:mx-0">
-                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
-                <p class="mt-3 text-lg leading-8 text-gray-300 justify-normal">Unlock Your Dream Job Today! With our vast selection of opportunities, we connect talented individuals like you with incredible career paths. Find the perfect job that aligns with your aspirations and takes your professional journey to new heights.</p>
+                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">ANNOUNCEMENTS</h2>
+                <p class="mt-3 text-lg leading-8 text-gray-300 justify-normal">Stay informed with our News Page! Get the latest updates on current events, 
+                    breaking news, and trending stories from around the world. Stay ahead of the curve and be in the know. Visit our News Page now!</p>
               </div>
 
 
               <!-- How it Work Part -->
-             <div>
-                <dl class="grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-4 lg:grid-cols-4">
-                
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CREATE ACCOUNT</h5>
-                        <p class="font-normal text-red-700 dark:text-red-400 ">REGISTER IN OUR JOB PORTAL</p>
-                    </a>
-
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">BUILD PROFILE</h5>
-                        <p class="font-normal text-red-700 dark:text-red-400 ">Create or Upload your Resume</p>
-                    </a>
-                    
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">FIND YOUR JOB</h5>
-                        <p class="font-normal text-red-700 dark:text-red-400 ">Browse for your dream job</p>
-                    </a>
-
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">SAVE AND APPLY</h5>
-                        <p class="font-normal text-red-700 dark:text-red-400 ">REGISTER IN OUR JOB PORTAL</p>
-                    </a>
-                    
-                 </dl>
-             </div>
+            
                 <!-- How it work -->
 
 
-              <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-
-                <!-- <div class="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-                  <a v-for="link in links" :key="link.name" :href="link.href">{{ link.name }} <span aria-hidden="true">&rarr;</span></a>
-                </div> -->
-                <h2 class="text-2xl font-bold tracking-tight text-white sm:text-2xl">SEARCH POSTED JOBS</h2>
-                
-                <dl class="mt-2 grid grid-cols-1 gap-8 sm:mt-2 sm:grid-cols-4 lg:grid-cols-4">
-                   
-            <label for="simple-search" class="sr-only">Job Position</label>
-            <div class="relative w-full">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                </div>
-                <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Job Position" required>
-            </div>
-
-            <label for="simple-search" class="sr-only">Search</label>
-            <div class="relative w-full">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                </div>
-                <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by company" required>
-            </div>
-
-
-<label for="simple-search" class="sr-only">Search</label>
-<div class="relative w-full">
-    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-    </div>
-    <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by category" required>
-</div>
-
-<!-- Search Magnifying Button -->
-
-
-<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px- rounded-full text-1xl">
-    SEARCH
-  </button>
-<!-- Search Magnifying Button -->
-
-<!-- <div v-for="stat in stats" :key="stat.name" class="flex flex-col-reverse">
-    <dt class="text-base leading-7 text-gray-300">{{ stat.name }}</dt>
-    <dd class="text-2xl font-bold leading-9 tracking-tight text-white">{{ stat.value }}</dd>
-  </div>  -->
-                
-                </dl>
-              </div>
 
 
             </div>
 
             
           </div>
-<!-- Second Container -->
-        <div class="mt-14">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-               
 
-                <div
-                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 
-                    dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
-                >
-                    <div>
-                    
-                        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">REGISTER</h2>
-                        <p class="mt-4 text-gray-200 dark:text-gray-200 text-md leading-relaxed">Discover a world of opportunities and take your career to new heights by registering as a job seeker on our innovative and user-friendly job portal website.</p>
-                        
-                        <button class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                            Register as Jobseeker
-                          </button>
-                    </div>
-                    
-                </div>
-                <div
-                class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 
-                dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
-            >
-                <div>
-                
-                    <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">EMPLOYER REGISTER</h2>
-                    <p class="mt-4 text-gray-200 dark:text-gray-200 text-md leading-relaxed">Unlock the power to find the perfect candidates for your organization's success by registering as an employer on our cutting-edge job portal website. 
-                        Streamline your hiring process and connect with top talent like never before.</p>
-                    
-                    <button class="mt-5 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">
-                        Register as Employer
-                      </button>
-                </div>
-                
-            </div>
-            
-            </div>
-        </div>
-<!-- Second Container -->
 
-<!-- Third Container -->
+<!-- Second Container -->
         <div class="mt-14">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                
@@ -197,7 +86,7 @@ defineProps({
                 >
                     <div>
                     
-                        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">NEWS FROM (MOIXIE)</h2>
+                        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">ANNOUNCEMENTS FROM (MOIXIE)</h2>
                         <p class="mt-4 text-gray-200 dark:text-gray-200 text-md leading-relaxed"> Stay informed with industry news, hiring trends, job market analysis, expert advice, and more. Empower your success as an employer or job seeker with our comprehensive coverage.</p>
                         
                        
@@ -229,15 +118,15 @@ defineProps({
 
                 <!-- News Modal use v-for-->
                     <div
-                    class="mt-7 scale-100 p-6 bg-white dark:bg-sky-500/30 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 
-                    dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+                    class="mt-7 scale-100 p-6 bg-white dark:bg-red-500/30 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 
+                    dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 border-2  border-l-red-500"
                 >
                     <div>
                     
                      
                         <div class="flex flex-col divide-y divide-y">
                       
-                            <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">HEADLINE</h2>
+                            <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">HEADLINES</h2>
                      
                             <div>
                                 <p class="mt-4 text-gray-200 dark:text-gray-200 text-md leading-relaxed">Discover a world of opportunities and take your career to new heights by registering as a job seeker on our innovative and user-friendly job portal website.</p>
@@ -267,12 +156,11 @@ defineProps({
             >
                 <div>
                 
-                    <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">5 LATEST JOBS POSTED</h2>
-                    <p class="mt-4 text-gray-200 dark:text-gray-200 text-md leading-relaxed">Discover the latest job opportunities on our portal. Connect with leading companies seeking professionals like you. 
-                        Explore diverse careers with our new postings.</p>
+                    <h2 class="text-4xl font-bold tracking-tight text-white sm:text-3xl">PREVIEW</h2>
+                   
                     
                         <div
-                        class="mt-7 scale-100 p-6 bg-white dark:bg-violet-400/30 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 
+                        class="mt-7 scale-100 p-6 bg-white dark:bg-sky-200/30 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 
                         dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                     >
                         <div>
@@ -301,7 +189,7 @@ defineProps({
             <!-- LATEST JOBS POSTED -->
             </div>
         </div>
-<!-- Third Container -->
+<!-- Second Container -->
 
 
 
