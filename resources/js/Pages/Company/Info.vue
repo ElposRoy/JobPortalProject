c8d55ea35f7cfca4b3e5640cd9ba166fa0115580
Moixie <royelpos@gmail.com>
2023-07-16 17:31:34 +0800

    Carousel Fixed

--- resources/js/Pages/Company/Info.vue
+++ resources/js/Pages/Company/Info.vue
@@ -30,17 +30,51 @@ defineProps({
 </script>
 
 <script>
+
+
+
+
     export default {
-        data () {
-            return ({
-                baseurl: location.origin,
-                carouselSlides: ['Pfp','image','Test'],
-            })
-        },
-    }
+  data() {
+    return {
+
+       
+
+      baseurl: location.origin,
+      carouselSlides: [
+        { title: 'Job-Type', key: 'Type' },
+        { title: 'Job-Type', key: 'Type' },
+        { title: 'Job-Type', key: 'Type' },
+        
+      ],
+      carouselCards: [
+        { title: 'Job-Type', key: 'Type' },
+        { position: 'Position', key: 'Job_position' },
+        { posted: 'Posted', key: 'Posted' },
+        
+      ],
+
+    
+    };
+  },
+  computed: {
+    totalCarouselCards() {
+        return Math.ceil(this.carouselCards.length / 3);
+      
+    },
+   
+  },
+
+  mounted() {
+   
+  },
+  
+};
 
 </script>
 
+
+v-for="(item, itemIndex) in slide" :key="itemIndex"
 <template>
     <Head title="Company-Info" />
   
@@ -269,34 +303,36 @@ defineProps({
             :pagination="true"
             :start-auto-play="true"
             :timeout="10000"
-            class="carousel " 
+            class="carousel" 
             v-slot="{currentSlide}"
             >
        
+            <!-- Carousel Cards is the numbers of how many slides or paginate -->
         <Slide v-for="(slide, index) in carouselSlides" :key="index">
             <div v-show="currentSlide === index + 1 " class="slide-info absolute flex justify-center items-center inset-0">
             
-                <div class="card-jobs m-5">
+        <div class="card-jobs m-5 w-full" v-for="(job, index) in carouselCards" :key="index">
     
-                    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     
-                        <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
-                        </svg>
-                        <a href="#">
-                            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{slide}}</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                        <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
-                            See our guideline
-                            <svg class="w-3 h-3 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                            </svg>
-                        </a>
-                    </div>
-    
+                <h5 class="mb-2 text-1xl font-semibold tracking-tight text-gray-400 ">Full-time</h5>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Personal Assistant</h5>
+                    <h5 class="mb-2 text-2lg font-semibold tracking-tight text-gray-800 dark:text-white">Pasig, Makati City</h5>
+                    <h5 class="mb-2 text-1xl font-semibold tracking-tight text-gray-400">4 Days ago</h5>
+                </a>
+
+                <div class="flex justify-center mt-5">
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 
+                    focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full 
+                    text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
+                    dark:focus:ring-blue-800">More Info</button>
                 </div>
-                <div class="card-jobs m-5">
+                
+            </div>
+    
+        </div>
+                <!-- <div class="card-jobs m-5">
     
                     <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     
@@ -337,7 +373,7 @@ defineProps({
                         </a>
                     </div>
     
-                </div>
+                </div> -->
     
             </div>
           </Slide>
