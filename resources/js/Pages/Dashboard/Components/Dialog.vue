4028a9b2ce947d69c801465bfef8102e1f658675
Moixie <royelpos@gmail.com>
2023-07-26 18:56:39 +0800

    Education Dialog

---
+++ resources/js/Pages/Dashboard/Components/Dialog.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+
+defineProps(['dialogEducation']);
+</script>
+<template>
+
+    <div class="dialog">
+
+      
+
+  <!-- Main modal -->
+  <div
+  id="staticModal"
+  data-modal-backdrop="static"
+  tabindex="-1"
+  class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex"
+  :class="{ 'hidden': !dialogEducation, 'modal-open': dialogEducation }"
+  aria-modal="true"
+  role="dialog"
+>
+  <div class="modal-overlay"></div> <!-- The modal overlay element -->
+  <div class="relative w-full lg:max-w-3xl max-h-full">
+    <slot/>
+  </div>
+</div>
+
+
+
+    </div>
+
+</template>
+
+
+ <script>
+ export default {
+ 
+ }
+ </script>
+ 
+ <style>
+
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.6); /* Adjust the opacity (last value) as needed */
+  z-index: -1; /* To position the overlay behind the modal content */
+}
+
+</style>
---
+++ resources/js/Pages/Dashboard/Components/Tertiary.vue
@@ -0,0 +1,91 @@
+<script setup>
+import Datepicker from 'flowbite-datepicker/Datepicker';
+
+
+
+defineProps(['dialogEducation']);
+defineEmits(['closeTertiaryDialog']);
+
+
+</script>
+<template>
+
+    <div class="dialog-content">
+
+      
+
+  
+  <!-- Modal content -->
+  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+    <!-- Modal header -->
+    <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            Tertiary Education
+        </h3>
+        <button   @click="$emit('closeTertiaryDialog')" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 
+        rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close modal</span>
+        </button>
+    </div>
+    <!-- Modal body -->
+    <div class="p-6 space-y-6">
+        <form>
+            <div class="grid gap-6 mb-6 md:grid-cols-5">
+                <div class="col-span-3">
+                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">School</label>
+                    <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+                    dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    placeholder="Ex: Computer Communication Development Institute" required>
+                </div>
+              
+                <div date-rangepicker class="flex items-center">
+                    <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                    </div>
+                    <input name="start" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date start">
+                    </div>
+                    <span class="mx-4 text-gray-500">to</span>
+                    <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                    </div>
+                    <input name="end" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date end">
+                </div>
+                </div>
+  
+              
+            </div>
+        </form>
+    </div>
+    <!-- Modal footer -->
+    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+        <button data-modal-hide="staticModal" 
+        type="button"
+         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+         font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Confirm</button>
+        </div>
+</div>
+
+  <slot/>
+
+    </div>
+
+</template>
+
+
+ <script>
+ 
+ export default {
+ 
+ }
+ </script>
+ 
+  
