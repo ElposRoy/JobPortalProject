fd7a7fb6ae64b3676ae78bd2bf7f77e0e73ca5f9
Moixie <royelpos@gmail.com>
2023-08-05 17:45:53 +0800

    Resume Skill Migration

---
+++ database/migrations/2023_08_05_053248_create_resume_skill_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resume_skill', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rsm_id')->constrained('resumes');
+            $table->string('skill_desc');
+           
+           
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resume_skill');
+    }
+};
