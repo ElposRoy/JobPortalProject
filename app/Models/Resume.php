fb38e63c3002ea31df3ec32826521a8c97bdab98
Moixie <royelpos@gmail.com>
2023-08-05 17:45:10 +0800

    Resume MOdel

---
+++ app/Models/Resume.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Resume extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable=[
+        'user_id',
+        'Image',
+        'Gender',
+        'Citizenship',
+        'LastName',
+        'FirstName',
+        'MiddleName',
+        'Suffix',
+        'Age',
+        'BirthDate',
+        'BirthPlace',
+        'BloodType',
+        'CivilStatus',
+        'Address',
+        'PhoneNumber',
+        'Email',
+        'CareerObjective',
+        'Weight',
+        'Height',
+
+        'Skill',
+        'Education',
+        'Experience',
+    ];
+
+    public function user(): BelongsTo
+
+    {
+
+        return $this->belongsTo(User::class);
+
+    }
+}
