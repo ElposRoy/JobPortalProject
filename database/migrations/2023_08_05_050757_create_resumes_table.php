9ceba55c685db8e363b1f21986c64ef8c31ba989
Moixie <royelpos@gmail.com>
2023-08-06 13:25:14 +0800

    New Database Column Names

--- database/migrations/2023_08_05_050757_create_resumes_table.php
+++ database/migrations/2023_08_05_050757_create_resumes_table.php
@@ -14,25 +14,25 @@ return new class extends Migration
         Schema::create('resumes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->string('rsm_pfp');
-            $table->string('rsm_email')->unique();
-            $table->string('rsm_fname');
-            $table->string('rsm_lname');
-            $table->string('rsm_mname');
-            $table->string('rsm_suffix')->nullable();
-            $table->tinyInteger('rsm_age');
-            $table->string('rsm_gender');
-            $table->string('rsm_bloodtype');
-            $table->string('rsm_height');
-            $table->string('rsm_weight');
+            $table->string('Image');
+            $table->string('Email')->unique();
+            $table->string('FirstName');
+            $table->string('LastName');
+            $table->string('MiddleName');
+            $table->string('Suffix')->nullable();
+            $table->tinyInteger('Age');
+            $table->string('Gender');
+            $table->string('BloodType');
+            $table->string('Height');
+            $table->string('Weight');
 
-            $table->date('rsm_birthday');
-            $table->string('rsm_birthplace');
-            $table->string('rsm_civilstatus');
-            $table->string('rsm_citizenship');
-            $table->string('rsm_address');
-            $table->string('rsm_phone');
-            $table->string('rsm_careerobj');
+            $table->date('BirthDate');
+            $table->string('BirthPlace');
+            $table->string('CivilStatus');
+            $table->string('Citizenship');
+            $table->string('Address');
+            $table->string('PhoneNumber');
+            $table->string('CareerObjective');
            
 
             $table->timestamp('created_at')->useCurrent();
