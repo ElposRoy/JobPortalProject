d86a5657f20b45987d69b83751fcd9f54f7b4764
Moixie <royelpos@gmail.com>
2023-08-06 13:25:28 +0800

    SkillInput changes

--- resources/js/Pages/Dashboard/Components/SkillInput.vue
+++ resources/js/Pages/Dashboard/Components/SkillInput.vue
@@ -95,7 +95,7 @@ defineEmits(['closeSkillsDialog','addSkill','clearSkills','removeSkill']);
       <!-- Modal footer -->
       <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
           <button 
-        
+          @click="$emit('closeSkillsDialog')"
           data-modal-hide="staticModal" 
           type="button"
            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
