d594313ee7284e22fc43ece1b9e7bcfb5b762192
Moixie <royelpos@gmail.com>
2023-08-07 16:26:52 +0800

    Adding Skill to Data Table

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Skill extends Model
 {
     use HasFactory;
+
+    protected $table = 'resume_skill';
+
+
+
+    protected $fillable=[
+        'rsm_id',
+        'Skill',
+      
+    ];
+
+    public function resume(): BelongsTo
+
+    {
+        return $this->belongsTo(Resume::class);
+
+    }
+
+
 }
