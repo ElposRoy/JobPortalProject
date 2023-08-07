f87f4c7495d860b8268e92aac23b1440d2949a95
Moixie <royelpos@gmail.com>
2023-08-07 16:26:34 +0800

    Adding Education to Data Table

--- app/Models/Education.php
+++ app/Models/Education.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Education extends Model
 {
     use HasFactory;
+
+    protected $table = 'resume_education';
+
+    protected $fillable=[
+        'rsm_id',
+        'Level',
+        'StartDate',
+        'EndDate',
+        'School',
+        'Address',
+        'Degree',
+        'Description',
+    ];
+
+    public function resume(): BelongsTo
+
+    {
+        return $this->belongsTo(Resume::class);
+
+    }
+
+
 }
