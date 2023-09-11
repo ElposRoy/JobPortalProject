629739c579cae30a956f4054a8925e86093fb68b
Moixie <royelpos@gmail.com>
2023-09-11 15:23:42 +0800

    Delete Button added

--- resources/js/Pages/Dashboard/Jobseeker-Page/Resume-Profile.vue
+++ resources/js/Pages/Dashboard/Jobseeker-Page/Resume-Profile.vue
@@ -331,6 +331,7 @@ try {
   onSuccess: () => {
     ExperienceForm.reset();
     this.dialogExperience = false;
+    this.hideJobEnded = false;
   },
 });
       
@@ -339,6 +340,30 @@ try {
       // Handle error
     }
   },
+
+  async deleteExperience(ID) {
+    // When Deleting a data, to submit delete ID { id: ID };
+
+   
+
+  try {
+
+    await this.$inertia.post(route('resume-profile.deleteExperience', { id: ID }), 
+    {
+  onSuccess: () => {
+  
+  },
+});
+
+
+
+  } catch (error) {
+    // Handle error
+    console.error(error);
+  }
+},
+
+
   async addNewSkill(SkillForm, ID , length) {
 
     try {
@@ -850,7 +875,7 @@ else{
           <div class="flex justify-between items-center">
             <button type="button" class="px-3 py-1 me-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg 
             hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
-            <button type="button" class="px-3 py-1 text-xs font-medium text-center text-white bg-red-700 rounded-lg 
+            <button @click="deleteExperience(experienceItem.id)" type="button" class="px-3 py-1 text-xs font-medium text-center text-white bg-red-700 rounded-lg 
             hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
             
            
